ec15629d010c5835461c2be69faca0cac1f47d00
Home <Home@Homes-MacBook-Pro.local>
2020-03-09 14:11:55 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,9 @@
+def square_number(number):
+    return number * number
+#DO NOT remove lines below here, this is designed to test your code.
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+print("YOUR CODE IS CORRECT!")
+test_square_number()
